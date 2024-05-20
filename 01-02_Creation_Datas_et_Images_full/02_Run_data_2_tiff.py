fe1e63b08237653122028e2b6935c147c802dcb3
Renaud HENRY <git@rhenry.fr>
2024-05-20 15:17:04 +0200

    OK

--- 01-02_Creation_Datas_et_Images_full/02_Run_data_2_tiff.py
+++ 01-02_Creation_Datas_et_Images_full/02_Run_data_2_tiff.py
@@ -1,12 +1,12 @@
 ####################  1. Les libraires  ####################
 
-# Librairie de gestion de tableau trés rapide
+# Librairie de gestion de tableau très rapide
 import numpy 
-# Librairie de gestion de création de fonction cuda diretement en python
+# Librairie de gestion de création de fonction cuda directement en python
 from numba import jit, cuda
-# Librairie de création et de maipulation d'images
+# Librairie de création et de manipulation d'images
 from PIL import Image
-# Librairie de gestion de chronometre
+# Librairie de gestion de chronomètre
 import time
 # Librairie de création de threads
 from joblib import Parallel, delayed
